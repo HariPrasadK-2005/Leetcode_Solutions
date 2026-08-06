c5a534cb2f16062f9602c4cefecae9c757639344
Hari Prasad K <hariprasadk182@gmail.com>
2026-08-06 15:27:02 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
-        //Arrays.fill(dp,-1);
-        dynamicProgramming(n,dp);
-        System.out.println(Arrays.toString(dp));
-        return dp[dp.length-1];
+        Arrays.fill(dp,-1);
+        int ans = dynamicProgramming(n,dp);
+        return ans;
     }
-    public void dynamicProgramming(int n , int[] dp){
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i=2; i<dp.length; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+    public int dynamicProgramming(int n , int[] dp){
+        if(n == 0 || n == 1){
+            return 1;
         }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = dynamicProgramming(n-1, dp) + dynamicProgramming(n-2, dp);
+        return dp[n];
     }
 }
