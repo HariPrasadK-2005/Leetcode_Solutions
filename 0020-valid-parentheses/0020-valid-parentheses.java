61a4aee7b561928c5aeec2a6336bbf8e24492a6e
Hari Prasad K <hariprasadk182@gmail.com>
2026-08-21 23:31:58 +0530

    Time: 3 ms (86.07%), Space: 43.5 MB (25.79%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                else{
+                    char top = stack.peek();
+                    if((top == '(' && c == ')') || (top == '[' && c == ']') || (top == '{' && c == '}')){
+                        stack.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        if(stack.empty()) return true;
+        else return false;
+    }
+}
